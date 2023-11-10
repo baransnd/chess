bb13a019795f8b052e6171037e2d592e338bfb5f
uboal <uboal@student.kit.edu>
2023-11-10 16:33:28 +0100

    gave movement to pieces

--- src/ChessBoard.py
+++ src/ChessBoard.py
@@ -1,3 +1,5 @@
+#ChessBoard.py
+
 import pygame
 from ChessLogic import GameState
 
@@ -5,21 +7,29 @@ IMAGES = {}
 BOARD_SIZE = 512
 SQUARE_SIZE = BOARD_SIZE / 8
 
+dragging_piece = None
+drag_start = None
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((BOARD_SIZE, BOARD_SIZE))
     pygame.display.set_caption("Chess Game")
     clock = pygame.time.Clock()
     gs = GameState()
-
     load_pieces()
+    
     running = True
     while (running):
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 running = False
+            elif e.type == pygame.MOUSEBUTTONDOWN:
+                handle_mouse_down(e, gs)
+            elif e.type == pygame.MOUSEBUTTONUP:
+                handle_mouse_up(e,gs)
         draw_board(screen)
         draw_pieces(screen, gs.board)
+        
         pygame.display.flip()
 
 
@@ -42,10 +52,8 @@ def draw_pieces(screen, board):
     for row in range(8):
         for column in range(8):
             piece = board[row][column]
-            if piece == "--":
-                continue
-            else:
-                screen.blit(IMAGES[piece], pygame.Rect(column * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            if piece != "--":
+                    screen.blit(IMAGES[piece], pygame.Rect(column * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
     """
     loads the pieces to a global list called IMAGES
@@ -56,13 +64,26 @@ def load_pieces():
     for piece in pieces:
         IMAGES[piece] = pygame.transform.scale(pygame.image.load("assets/pieces/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
 
+def handle_mouse_down(event, gs):
+    global dragging_piece, drag_start
+    if event.button == 1:  # Left mouse button
+        col = int(event.pos[0] // SQUARE_SIZE)
+        row = int(event.pos[1] // SQUARE_SIZE)
+        dragging_piece = gs.board[row][col]
+        drag_start = (row, col)
 
+def handle_mouse_up(event, gs):
+    global dragging_piece, drag_start  # Use global variables
+    if event.button == 1 and dragging_piece != "--":
+        col = int(event.pos[0] // SQUARE_SIZE)
+        row = int(event.pos[1] // SQUARE_SIZE)
+        end_position = (row, col)
+        gs.move_piece(drag_start, end_position)
+        dragging_piece = None
+        drag_start = None  # Reset drag_start after the move
         
 if __name__ == "__main__":
     main()
-
-            
-def add:
     
 
 
--- src/ChessLogic.py
+++ src/ChessLogic.py
@@ -12,3 +12,10 @@ class GameState():
                       ["white-rook", "white-knight","white-bishop","white-queen","white-king","white-bishop", "white-knight","white-rook"]]
         self.white_to_move = True
         self.moveLog = []
+        
+    def move_piece(self, start, end):
+        start_row, start_column = start
+        end_row, end_column = end
+        self.board[end_row][end_column] = self.board[start_row][start_column]
+        self.board[start_row][start_column] ="--"
+        
